de96fd94543d091f6d325d2be10f1f1b4078fa9b
katerina07088 <ekaterinakorotenko@yandex.ru>
2024-05-03 08:08:25 +0300

    done

--- main.js
+++ main.js
@@ -2,16 +2,11 @@
 
 import { getComments, postComments } from "./api.js";
 import { renderComments } from "./render.js";
-import { sanitize,deleteComment } from "./manipulationsWithComments.js"; 
+import { addComment, deleteComment } from "./manipulationsWithComments.js"; 
+
 
 
   // элементы
- 
-  const addFormButtonEl = document.getElementById('addFormButton');
-  const inputNameEl = document.getElementById('inputName');
-  const textCommentEl = document.getElementById('textComment'); // здесь и в manipulationwithComents
-  //const deleteButtonEl = document.getElementById('delete-button');
-  const addFormEl = document.querySelector('add-form')
   const commentHiddenEl = document.getElementById('commentHidden');
 
 
@@ -19,8 +14,7 @@ import { sanitize,deleteComment } from "./manipulationsWithComments.js";
   let comments = [];
 
   // функция на получение данных из API
-
-  function getFetchPromise() {
+  export function getFetchPromise() {
     commentHiddenEl.classList.remove('comment-hidden');
     const fetchPromise = getComments()
       .then((reponseData) => {
@@ -36,67 +30,13 @@ import { sanitize,deleteComment } from "./manipulationsWithComments.js";
         comments = appComments
         commentHiddenEl.classList.add('comment-hidden');
         renderComments({comments})
+        deleteComment({comments});
       })
       .catch((error) => {
         alert(error.message);
       })
   };
   getFetchPromise();
-
-  //renderComments({comments});
-  deleteComment({comments});
-
-
-  // обработчик кнопки написать комментарий
-  addFormButtonEl.addEventListener('click', function (e) {
-    e.stopPropagation();
-
-    inputNameEl.classList.remove('error');
-    textCommentEl.classList.remove('error');
-    if (inputNameEl.value.trim() === '' && textCommentEl.value.trim() === '') {
-      inputNameEl.classList.add('error');
-      textCommentEl.classList.add('error');
-      return;
-    } if (inputNameEl.value.trim() === '') {
-      inputNameEl.classList.add('error');
-      return;
-    } else if (textCommentEl.value.trim() === '') {
-      textCommentEl.classList.add('error');
-      return;
-    }
-
-    addFormButtonEl.disabled = true;
-    addFormButtonEl.textContent = "Комментарий добавляется...";
-
-    const postFetch = postComments({
-      name: sanitize(inputNameEl.value),
-      text: sanitize(textCommentEl.value)} )
-      .then(() => {
-        getFetchPromise();
-        addFormButtonEl.disabled = false;
-        addFormButtonEl.textContent = "Написать";
-        inputNameEl.value = "";
-        textCommentEl.value = "";
-      })
-      .catch((error) => {
-        addFormButtonEl.disabled = false;
-        addFormButtonEl.textContent = "Написать";
-        alert(error.message);
-        console.warn(error);
-      })
-  });
-  
-
-  //добавление комментария через enter 
-  // inputNameEl.addEventListener('keyup', (event) => {
-  //   if (event.which === 13) {
-  //       addFormButtonEl.click();
-  //       }
-  //  })
-  //  textCommentEl.addEventListener('keyup', (event) => {
-  //   if (event.which === 13) {
-  //     addFormButtonEl.click();
-  //       }
-  //  })
+  addComment();
 
   console.log("It works!");
--- manipulationsWithComments.js
+++ manipulationsWithComments.js
@@ -1,7 +1,53 @@
 import { renderComments } from "./render.js";
+import { postComments } from "./api.js";
+import { getFetchPromise } from "./main.js";
+
+const inputNameEl = document.getElementById('inputName');
 const textCommentEl = document.getElementById('textComment');
 const addFormButtonEl = document.getElementById('addFormButton');
 const deleteButtonEl = document.getElementById('delete-button');
+
+// обработчик кнопки написать комментарий
+export function addComment() {
+    addFormButtonEl.addEventListener('click', function (e) {
+    e.stopPropagation();
+
+    inputNameEl.classList.remove('error');
+    textCommentEl.classList.remove('error');
+    if (inputNameEl.value.trim() === '' && textCommentEl.value.trim() === '') {
+      inputNameEl.classList.add('error');
+      textCommentEl.classList.add('error');
+      return;
+    } if (inputNameEl.value.trim() === '') {
+      inputNameEl.classList.add('error');
+      return;
+    } else if (textCommentEl.value.trim() === '') {
+      textCommentEl.classList.add('error');
+      return;
+    }
+
+    addFormButtonEl.disabled = true;
+    addFormButtonEl.textContent = "Комментарий добавляется...";
+
+    const postFetch = postComments({
+      name: sanitize(inputNameEl.value),
+      text: sanitize(textCommentEl.value)} )
+      .then(() => {
+        getFetchPromise();
+        addFormButtonEl.disabled = false;
+        addFormButtonEl.textContent = "Написать";
+        inputNameEl.value = "";
+        textCommentEl.value = "";
+      })
+      .catch((error) => {
+        addFormButtonEl.disabled = false;
+        addFormButtonEl.textContent = "Написать";
+        alert(error.message);
+        console.warn(error);
+      })
+  });
+}
+
 
 
 //функция ответа на комментарий
